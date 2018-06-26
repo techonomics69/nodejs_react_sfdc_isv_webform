d9f6f68382e4e47e91282a431f7e3ebb09982918
Courtney Dascenzo <cdascenzo@cdascenzo-ltrm.internal.salesforce.com>
2018-06-26 12:18:17 -0400

    updated get newTrial

--- react-ui/src/containers/TrialForm.js
+++ react-ui/src/containers/TrialForm.js
@@ -77,7 +77,7 @@ class TrialForm extends React.Component {
     });
 
     Object.keys(this.state.item).map((i) => {
-    	if(this.state.item[i] == ''){
+    	if(this.state.item[i] == '' || this.state.item[i] == null){
     		var e = [i]+'Error';
     		this.setState({[e]: 'This is a required field.',})
     	}
